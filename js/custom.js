5055e3c2e7c73990951ad7911af6255cf523d66d
Kamran-Chang <kamranchang22@gmail.com>
2024-05-16 11:03:48 +0500

    new Update

--- js/custom.js
+++ js/custom.js
@@ -77,5 +77,10 @@ window.onscroll = () => {
     let offset = sec.offsetTop;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+      });
+    }
   });
 };
